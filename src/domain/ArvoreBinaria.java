d77774ee8eaeb11131ee93ce549a0a6fc62e0578
WellingtonEugenio <wellingtoneugenio@gmail.com>
2024-08-21 17:54:11 -0300

    Criando novas funcoes

--- src/domain/ArvoreBinaria.java
+++ src/domain/ArvoreBinaria.java
@@ -8,6 +8,38 @@ public class ArvoreBinaria <T extends Comparable<T>>{
 		this.raiz = null;
 	}
 	
+	public void salvarNo(T conteudo) {
+		BinNo<T> novoNo = new BinNo<>(conteudo);
+		raiz = inserir(raiz, novoNo);
+	}
 	
+	private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo){
+		//
+		if(atual == null) {
+			return novoNo;
+		} else if(novoNo.getConteudo().compareTo(atual.getConteudo()) < 0) {
+			atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
+		}else {
+			atual.setNoDir(inserir(atual.getNoDir(), novoNo));
+		}
+		
+		return atual;
+	}
 
+	public void exibirInOrdem() {
+		//
+		System.out.println("\n Exibindo InOrdem");
+		inOrdem(this.raiz);
+	}
+
+	private void inOrdem(BinNo<T> atual) {
+		//
+		if(atual != null) {
+			inOrdem
+		}
+		
+	}
+	
+	
+	
 }
